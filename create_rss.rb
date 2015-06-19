7ae41a44222e4a04a07907186300ce8db3dd0535
Chris McKenzie <kristopolous@yahoo.com>
2015-06-19 13:06:06 -0700

    adding an indented version and more ruby code to generate it

--- create_rss.rb
+++ create_rss.rb
@@ -3,9 +3,26 @@ require 'builder'
 
 def product_xml
   xml = Builder::XmlMarkup.new( :indent => 2 )
-  xml.instruct! :xml, :encoding => "ASCII"
-  xml.product do |p|
-    p.name "Test"
+  xml.rss( "xmlns:dc" => "http://purl.org/dc/elements/1.1/", 
+           "xmlns:media" => "http://search.yahoo.com/mrss/",
+           "xmlns:itunes" => "http://www.itunes.com/dtds/podcast-1.0.dtd", 
+           "xmlns:feedburner" => "http://rssnamespace.org/feedburner/ext/1.0",
+           "version" => "2.0",
+           "xml:base" => "http://www.againstthegrain.org"
+         ) do
+
+    xml.channel do | channel |
+      channel.title "Some title"
+      channel.link "http://somelink"
+      channel.description "Some desc"
+      channel.language "en"
+    
+      channel.node do | l |
+        l.name "Hi"
+      end
+      channel.name "Test"
+
+    end
   end
 end
 
