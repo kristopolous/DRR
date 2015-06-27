e8eff9fbeeb9cdae9c138e4d3aa4192c70a7e23e
Chris McKenzie <kristopolous@yahoo.com>
2015-06-26 17:07:53 -0700

    modes

--- server/indy_server.py
+++ server/indy_server.py
@@ -49,11 +49,28 @@ def to_minute(unix_time):
 
   return unix_time.weekday() * (24 * 60) + unix_time.hour * 60 + unix_time.minute
 
-def now():
+def minute_now():
   return to_minute(datetime.utcnow())
 
 def ago(duration):
   return time.time() - duration
+
+# From https://wiki.python.org/moin/ConfigParserExamples
+def ConfigSectionMap(section, Config):
+  dict1 = {}
+  options = Config.options(section)
+
+  for option in options:
+    try:
+      dict1[option] = Config.get(section, option)
+      if dict1[option] == -1:
+        logging.info("skip: %s" % option)
+
+    except:
+      logging.warning("exception on %s!" % option)
+      dict1[option] = None
+
+  return dict1  
 
 # This takes the nominal weekday (sun, mon, tue, wed, thu, fri, sat)
 # and a 12 hour time hh:mm [ap]m and converts it to our absolute units
@@ -63,6 +80,7 @@ def to_utc(day_str, hour):
 
   try:
     day_number = ['sun','mon','tue','wed','thu','fri','sat','sun'].index(day_str.lower())
+
   except e:
     return False
 
@@ -83,6 +101,7 @@ def to_utc(day_str, hour):
   utc = local + g_config['offset']
 
   return utc
+
 
 def get_time_offset():
   global g_config
@@ -105,6 +124,7 @@ def get_time_offset():
 
   return False
 
+
 def db_connect():
   global g_db
 
@@ -126,6 +146,7 @@ def db_connect():
 
   return g_db
 
+
 def register_intent(minute, duration):
   db = db_connect()
 
@@ -142,12 +163,13 @@ def register_intent(minute, duration):
   db['conn'].commit()
   return db['c'].lastrowid
   
+
 def should_be_recording():
   global g_config
 
   db = db_connect()
 
-  current_minute = now()
+  current_minute = minute_now()
 
   intent_count = db['c'].execute(
     """select count(*) from intents where 
@@ -160,6 +182,7 @@ def should_be_recording():
 
   return intent_count != 0
   
+
 def prune():
   global g_config
 
@@ -230,6 +253,7 @@ def find_streams(start_query, duration):
 def generate_xml(showname):
   return True
 
+
 def server():
   app = Flask(__name__)
 
@@ -259,6 +283,7 @@ def server():
     return weekday + start + duration + name
 
   app.run(debug=True)
+
 
 def download(callsign, url):
 
@@ -293,25 +318,35 @@ def download(callsign, url):
 def spawner():
   global g_queue, g_config
 
-  station = {
-    'callsign': g_config['callsign'],
-    'url': g_config['stream']
-  }
-
   last = {
     'prune': 0,
     'offset': 0
   }
 
+  callsign = g_config['callsign']
+  url = g_config['stream']
   day = 24 * 60 * 60
+  mode_full = (g_config['mode'].lower() == 'full')
   b_shutdown = False
-  b_flag = False
+  should_record = mode_full
+
+  # Number of seconds to be cycling
+  cycle_time = 5
+
   process = False
 
   server_pid = Process(target=server)
   server_pid.start()
 
   while True:
+
+    # We cycle this to off for every run.
+    # By the time we go throug the queue
+    # so long as we aren't supposed to be
+    # shutting down, this should be toggled
+    # to true
+    #
+    flag = False
 
     if last['prune'] < ago(1 * day):
       prune()
@@ -327,42 +362,42 @@ def spawner():
       if data == 'shutdown':
         b_shutdown = True
       else:
-        b_flag = True
+        flag = True
     
-    # didn't respond in 3 seconds so we respawn
-    if b_flag == False:
+    # If we are not in full mode, then we should check
+    # whether we should be recording right now according
+    # to our intents.
+    if not mode_full:
+      should_record = should_be_recording()
+
+    if should_record:
+
+      # Didn't respond in cycle_time seconds so we respawn
+      if not flag:
+        if process != False and process.is_alive():
+          process.terminate()
+        process = False
+
+      if not process and not b_shutdown:
+        process = Process(target=download, args=(callsign, url,))
+        process.start()
+
+      # If there is still no process then we should definitely bail.
+      if not process:
+        return False
+
+    # The only way for the bool to be toggled off
+    # is if we are not in full-mode ... we get here
+    # if we should NOT be recording.  So we make sure
+    # we aren't.
+    else:
       if process != False and process.is_alive():
         process.terminate()
       process = False
 
-    if process == False and b_shutdown == False:
-      process = Process(target=download, args=(g_config['callsign'], station['url'],))
-      process.start()
+    time.sleep(cycle_time)
 
-    # If there is still no process then we should definitely bail.
-    if process == False:
-      return False
 
-    b_flag = False
-
-    time.sleep(3)
-
-# From https://wiki.python.org/moin/ConfigParserExamples
-def ConfigSectionMap(section, Config):
-  dict1 = {}
-  options = Config.options(section)
-
-  for option in options:
-    try:
-      dict1[option] = Config.get(section, option)
-      if dict1[option] == -1:
-        logging.info("skip: %s" % option)
-
-    except:
-      logging.warning("exception on %s!" % option)
-      dict1[option] = None
-
-  return dict1  
 
 def startup():
   global g_config
