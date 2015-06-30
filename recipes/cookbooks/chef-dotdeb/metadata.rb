259c000ebe19093f8203d13cd276d82f1bf33af2
Chris McKenzie <kristopolous@yahoo.com>
2015-06-29 18:10:58 -0700

    more cleanup

--- recipes/cookbooks/chef-dotdeb/metadata.rb
+++
@@ -1,6 +0,0 @@
-maintainer       "Benedict Steele"
-maintainer_email "operations@homemadedigital.com"
-license          "Apache 2.0"
-description      "Installs/Configures dotdeb"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
