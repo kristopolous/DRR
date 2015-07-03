6526b0995fa2f4a0a1cad7a12736f420509166bd
Chris McKenzie <kristopolous@yahoo.com>
2015-07-02 23:08:15 -0700

    slice successful

--- server/audio_hopper.py
+++ server/audio_hopper.py
@@ -14,11 +14,11 @@ def mp3_crc(fname, blockcount = -1):
     224, 256, 320, 0
   ]
 
-
   f = open(fname, 'rb')
   while blockcount != 0:
     blockcount -= 1
 
+    frame_start = f.tell()
     header = f.read(2)
     if header:
 
@@ -27,7 +27,6 @@ def mp3_crc(fname, blockcount = -1):
         samp_rate = freqTable[(b & 0x0f) >> 2]
         bit_rate = brTable[b >> 4]
         pad_bit = (b & 0x3) >> 1
-        frame_start = f.tell() - 3
         # from http://id3.org/mp3Frame
         frame_size = (144000 * bit_rate / samp_rate) + pad_bit
 
@@ -38,7 +37,7 @@ def mp3_crc(fname, blockcount = -1):
         frame_sig.append(binascii.crc32(f.read(32)))
         start_byte.append(frame_start)
 
-        # Move forward the frame
+        # Move forward the frame f.read size + 4 byte header
         throw_away = f.read(frame_size - 36)
 
       else:
@@ -55,31 +54,29 @@ def serialize(file1, file1_stop, file2, file2_start):
   f = open(file1, 'rb')
 
   if file1_stop == 0:
-    part1 = f.read()
+    out.write(f.read())
   else:
-    part1 = f.read(file1_stop)
+    out.write(f.read(file1_stop))
 
   f.close()
 
   f = open(file2, 'rb')
   f.seek(file2_start)
-  part2 = f.read()
+  out.write(f.read())
 
   f.close()
-
-  out.write(part1 + part2)
+  out.close()
   return True
 
 def stitch_attempt(first, second):
-  crc32_first = mp3_crc(first)
-  crc32_second = mp3_crc(second, 2000)
+  crc32_first, offset_first = mp3_crc(first)
+  crc32_second, offset_second = mp3_crc(second, 2000)
 
-  ordinal = []
   last = 0
   isFound = True
 
   for i in xrange(5, 1, -1):
-    pos = crc32_second[0].index(crc32_first[0][-i])
+    pos = crc32_second.index(crc32_first[-i])
     if last != 0 and pos - last != 1:
       isFound = False
 
@@ -88,7 +85,7 @@ def stitch_attempt(first, second):
   # Since we end at the last block, we can safely pass in a file1_stop of 0
   if isFound:
     # And then we take the offset in the crc32_second where things began, + 1
-    serialize(first, 0, second, crc32_second[1][last + 1])
+    serialize(first, 0, second, offset_second[last + 1])
 
   print isFound
 
