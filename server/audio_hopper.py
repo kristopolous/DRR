431b9fb0a29449d06fd271f460004b99ab6b627d
Chris McKenzie <kristopolous@yahoo.com>
2015-07-02 20:54:45 -0700

    should i programming in python all my life?

--- server/audio_hopper.py
+++ server/audio_hopper.py
@@ -2,11 +2,22 @@
 import binascii
 
 with open('../misc/test.mp3', 'rb') as f:
+
+  brTable = [
+    0,   32,  40,  48, 
+    56,  64,  80,  96, 
+    112, 128, 160, 192, 
+    224, 256, 320, 0
+  ]
+
   while True:
-    chunk = f.read(1)
-    if chunk:
-      print chunk[0] == '\xff'
-      print "%s" % binascii.b2a_hex(chunk)
+    header = f.read(2)
+    if header:
+      if header == '\xff\xfb':
+        b = ord(f.read(1))
+        print (b & 0x0f) >> 2
+        print freqTable[b >> 4]
+        #print "%s" % binascii.b2a_hex(chunk)
     else:
       break
 
