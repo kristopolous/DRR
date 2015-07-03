11cf6c12aae1a2fd7da8ec9f997883b233e2748d
Chris McKenzie <kristopolous@yahoo.com>
2015-07-02 18:19:38 -0700

    bin comparison

--- server/audio_hopper.py
+++ server/audio_hopper.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python -O
+import binascii
+
+with open('../misc/test.mp3', 'rb') as f:
+  while True:
+    chunk = f.read(1)
+    if chunk:
+      print chunk[0] == '\xff'
+      print "%s" % binascii.b2a_hex(chunk)
+    else:
+      break
+
