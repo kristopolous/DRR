5143348ff49b967b172ef07eec86764b57a96d5a
Chris McKenzie <kristopolous@yahoo.com>
2015-07-07 14:54:48 -0700

    more management infrastructure

--- html/db.php
+++ html/db.php
@@ -1,5 +1,19 @@
 <?
 $db = new SQLite3("main.db");
+$read_only = ($_SERVER['REMOTE_ADDR'] !== '::1');
+$schema = [
+  'id'          => 'INTEGER PRIMARY KEY', 
+  'callsign'    => 'TEXT',
+  'description' => 'TEXT',
+  'base_url'    => 'TEXT',
+  'last_seen'   => 'TIMESTAMP DEFAULT CURRENT_TIMESTAMP',
+  'first_seen'  => 'TIMESTAMP DEFAULT CURRENT_TIMESTAMP',
+  'pings'       => 'INTEGER',
+  'drops'       => 'INTEGER',
+  'latency'     => 'INTEGER',
+  'log'         => 'TEXT',
+  'notes'       => 'TEXT'
+];
 
 function prune($obj) {
   $ret = $obj->fetchArray();
@@ -11,5 +25,21 @@ function prune($obj) {
     }
   } 
   return $ret;
+}
+
+function sql_escape_hash($obj) {
+  $res = [];
+  foreach($obj as $key => $value) {
+    $res[SQLite3::escapeString($key)] = SQLite3::escapeString($value);
+  }
+  return $res;
+}
+
+function add_station($dirty) {
+  global $db;
+  $clean = sql_escape_hash($dirty);
+
+  $db->exec('select * from stations where callsign = "' . $clean['callsign'] . '"');
+  var_dump($var);
 }
 
