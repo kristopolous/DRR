bdbf93e33e09d58764920148ae8bba8d1447e9ef
Chris McKenzie <kristopolous@yahoo.com>
2015-07-07 00:47:00 -0700

    installerg

---
+++ bootstrap.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+cd server
+install() {
+  sudo apt-get -y -f install  \
+      python-pip   python       \
+      python-dev   libxslt1-dev \
+      libxml2-dev  python-pycurl 
+
+  pip install --user -r requirements.txt
+}
+
+install
