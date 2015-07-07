52a4859a5d48ac63f96ab356d8145ceea2afe756
Chris McKenzie <kristopolous@yahoo.com>
2015-07-07 16:32:55 -0700

    more admin backend

--- html/modify.php
+++ html/modify.php
@@ -1,17 +1,30 @@
 <?php
 include_once('db.php');
+
 if($_SERVER['REQUEST_METHOD'] == 'POST') {
-  $res = add_station($_POST);
+  if(isset($_POST['_action'])) {
+    unset($_POST['_action']);
+    $res = del_station($_POST);
+  } else {
+    $_POST['active'] = 1;
+    $res = add_station($_POST);
+  }
   header('Location: ' . dirname($_SERVER['REQUEST_URI']) . '/admin.php');
   exit(0);
 }
 
+$station = false;
 if(!empty($_GET['id'])) {
   $station = get_station(['id' => $_GET['id']]);
-  if(!$station) { 
-    $station = [];
-  }
 }
+
+if (!$station) { 
+  $station = [];
+  echo '<title>new station | indycast</title>';
+} else {
+  echo '<title>' . $station['callsign'] . ' modify | indycast</title>';
+}
+
 ?>
 <style>
 form { display: inline-block }
@@ -33,6 +46,13 @@ label { width: 10em;display: inline-block;}
     }
   ?>
   <input type='submit' value='Add/Modify station'>
+  <a href="admin.php">cancel</a>
 </form>
-
-
+<?php if (!empty($station)) { ?>
+<br/>
+<form method="post">
+  <input type='hidden' name='id' value='<?= $station['id'] ?>'>
+  <input type='hidden' value='delete' name='_action'>
+  <input type='submit' value='Delete <?= $station['callsign'] ?>'>
+</form>
+<?php } ?>
