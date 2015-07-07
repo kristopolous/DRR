b208a5c542eb48b49dbd6f352011fdc2dde4d8e2
Chris McKenzie <kristopolous@yahoo.com>
2015-07-07 16:41:27 -0700

    url rewriting based on the db

--- html/forwarder.php
+++ html/forwarder.php
@@ -1,3 +1,10 @@
 <?php
 include_once('db.php');
-var_dump($_SERVER);
+$parts = explode('/', $_SERVER['QUERY_STRING']);
+$callsign = array_shift($parts);
+$request = implode('/', $parts);
+$station = get_station(['callsign' => $callsign]);
+
+if($station) {
+  header('Location: http://' . $station['base_url'] . '/' . $request);
+}
