13b36941d8084b18fcde5ca3c2e40f0617b6e895
Chris McKenzie <kristopolous@yahoo.com>
2015-07-07 15:59:12 -0700

    more backend

--- html/modify.php
+++ html/modify.php
@@ -1,10 +1,17 @@
 <?php
 include_once('db.php');
 if($_SERVER['REQUEST_METHOD'] == 'POST') {
-  add_station($_POST);
+  $res = add_station($_POST);
+  header('Location: ' . dirname($_SERVER['REQUEST_URI']) . '/admin.php');
   exit(0);
 }
 
+if(!empty($_GET['id'])) {
+  $station = get_station(['id' => $_GET['id']]);
+  if(!$station) { 
+    $station = [];
+  }
+}
 ?>
 <style>
 form { display: inline-block }
@@ -16,11 +23,16 @@ label { width: 10em;display: inline-block;}
   <?php
     foreach($schema as $key => $value) {
       if($value == 'TEXT') {
-        echo "<div><label for='$key'>$key</label><input type='text' id='$key' name='$key'></input></div>";
+        echo "<div><label for='$key'>$key</label>";
+        echo "<input 
+          value='" . (isset($station[$key]) ? $station[$key] : '') . "'
+          type='text' 
+          id='$key' name='$key' />
+         </div>";
       }
     }
   ?>
-  <input type='submit' value='Add station'>
+  <input type='submit' value='Add/Modify station'>
 </form>
 
 
