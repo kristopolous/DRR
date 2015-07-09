14a308fdc1c200dc89e961cc88d612d946b63531
Chris McKenzie <kristopolous@yahoo.com>
2015-07-08 21:32:42 -0700

    server health cleanup

--- server/server_health.py
+++ server/server_health.py
@@ -17,31 +17,13 @@ def getAddrInfoWrapper(host, port, family=0, socktype=0, proto=0, flags=0):
 socket.getaddrinfo = getAddrInfoWrapper
 
 import urllib2
-
 import sqlite3
 import time
 
-g_db = {}
+conn = sqlite3.connect('../db/main.db')
+db = {'conn': conn, 'c': conn.cursor()}
 
-def db_connect():
-  """
-  a "singleton pattern" or some other fancy $10-world style of maintaining 
-  the database connection throughout the execution of the script.
-
-  Returns the database instance
-  """
-
-  global g_db
-
-  if 'conn' not in g_db:
-    conn = sqlite3.connect('../db/main.db')
-    g_db = {'conn': conn, 'c': conn.cursor()}
-
-  return g_db
-
-db = db_connect()
-
-cycle_time = 60 * 60 * 12
+CYCLE_TIME = 60 * 60 * 12
 
 # this trick robbed from http://stackoverflow.com/questions/702834/whats-the-common-practice-for-enums-in-python
 ID, CALLSIGN, DESCRIPTION, BASE_URL, LAST_SEEN, FIRST_SEEN, PINGS, DROPS, LATENCY, ACTIVE, LOG, NOTES = range(12)
@@ -50,17 +32,24 @@ while True:
   # retrieve a list of the active stations
   station_list = db['c'].execute('select * from stations where active == 1')
 
-  for station in station_list:
+  for station in station_list.fetchall():
     url = station[BASE_URL]
+
     try:
-      stream = urllib2.urlopen("http://%s/stats" % url)
+      start = time.time()
+
+      stream = urllib2.urlopen("http://%s/heartbeat" % url)
+      data = stream.read()
+
+      stop = time.time()
+
+      print url
+      print data
+      db['c'].execute('update stations set latency = latency + ?, pings = pings + 1, last_seen = current_timestamp where id = ?', ( str(stop - start), str(station[ID]) ))
 
     except urllib2.HTTPError:
       # Say that we couldn't see this station
       db['c'].execute('update stations set drops = drops + 1 where id = ?', str(station[ID]))
-      db['conn'].commit()
 
-    data = stream.read()
-    print data
-
-  time.sleep(cycle_time)
+  db['conn'].commit()
+  time.sleep(CYCLE_TIME)
