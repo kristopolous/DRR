449ff3ba93206d9a873d19eeb5ce6e1504811d77
Chris McKenzie <kristopolous@yahoo.com>
2015-07-09 05:49:29 -0700

    mime type fix again

--- server/indy_server.py
+++ server/indy_server.py
@@ -849,7 +849,7 @@ def server_generate_xml(showname, feed_list, duration, start_minute, weekday, st
 
   tree = ET.ElementTree(root)
 
-  return Response(ET.tostring(tree, xml_declaration=True, encoding="utf-8"), mimetype = 'text/xml; charset=UTF-8')
+  return Response(ET.tostring(tree, xml_declaration=True, encoding="utf-8"), mimetype = 'text/xml')
 
 
 
