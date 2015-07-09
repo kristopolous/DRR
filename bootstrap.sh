ec3da98ad8c842cca31413eb36c1309cd7097b6b
Chris McKenzie <kristopolous@yahoo.com>
2015-07-09 03:41:19 -0700

    simplifying bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
 cd server
-install() {
-  sudo apt-get update
-  sudo apt-get -y -f install  \
-      python-pip   python       \
-      python-dev   libxslt1-dev \
-      libxml2-dev  python-pycurl 
 
-  pip install --user -r requirements.txt
-}
+sudo apt-get update
+sudo apt-get -y -f install    \
+    python-pip   python       \
+    python-dev   libxslt1-dev \
+    libxml2-dev  python-pycurl 
 
-install
+pip install --user -r requirements.txt
