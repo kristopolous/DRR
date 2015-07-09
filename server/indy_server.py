ed32a7fe3aa25f174098d0352ff812243ff13b4d
Chris McKenzie <kristopolous@yahoo.com>
2015-07-09 01:14:16 -0700

    site-map

--- server/indy_server.py
+++ server/indy_server.py
@@ -862,6 +862,18 @@ def server_manager(config):
       raise RuntimeError('Not running with the Werkzeug Server')
     func()
 
+  # From http://stackoverflow.com/questions/13317536/get-a-list-of-all-routes-defined-in-the-app
+  @app.route("/site-map")
+  def site_map():
+    links = []
+    for rule in app.url_map.iter_rules():
+        # Filter out rules we can't navigate to in a browser
+        # and rules that require parameters
+        if "GET" in rule.methods and has_no_empty_params(rule):
+            url = url_for(rule.endpoint)
+            links.append((url, rule.endpoint))
+
+    return jsonify(links), 200
   #
   # The path is (unix timestamp)_(duration in minutes). If it exists (as in we had 
   # previously generated it) then we can trivially send it.  Otherwise we need
