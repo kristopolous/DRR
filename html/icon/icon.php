a01285085c13f66c0ba1d41f15b325f9cabb5a6b
Chris McKenzie <kristopolous@yahoo.com>
2015-07-10 17:30:17 -0700

    icon work

--- html/icon/icon.php
+++ html/icon/icon.php
@@ -17,11 +17,12 @@ $out_res = 512;
 
 if(count($matches)) {
   $out_res = intval($matches[1]);
+  $show = substr($show, 0, strrpos($show, '_'));
 } 
-var_dump($out_res);
 
 $height = 1715;
 $width = 1715;
+$image = new Imagick('../images/radio-backdrop_1715.png');
 
 $draw = new ImagickDraw();
 
@@ -40,7 +41,7 @@ foreach ($wordList as $word) {
   $maxLength = max(strlen($word), $maxLength);
 }
 
-$fontsize = 420;
+$fontsize = 410;
 if($maxLength > 6) {
   $fontsize = (6 / $maxLength) * $fontsize;
 }
@@ -48,18 +49,13 @@ if($maxLength > 6) {
 $draw->setFontSize( $fontsize );
 
 $draw->setStrokeColor('#000');
-$draw->setStrokeWidth(4);
+$draw->setStrokeWidth(6);
 $draw->setStrokeAntialias(true);  //try with and without
 $draw->setTextAntialias(true);  //try with and without
-$draw->setGravity(imagick::GRAVITY_SOUTH);
-$outputImage = new Imagick();
-$outputImage->newImage($height, $width, "transparent");  //transparent canvas
-$outputImage->annotateImage($draw, 0, 0, 0, $show);
-$outputImage->trimImage(0); //Cut off transparent border
+$draw->setGravity(imagick::GRAVITY_NORTH);
 //$outputImage->resizeImage(300,0, imagick::FILTER_CATROM, 0.9, false);
 
 
-$image = new Imagick('../images/radio-backdrop_1715.png');
 
 $map = 'abcdefghijklmnopqrstuvwxyz';
 
@@ -71,12 +67,10 @@ $hue = floor($offset / (26 * 26) * 360);
 $color = new ImagickPixel("hsl($hue, 150, 110)");
 $image->colorizeImage($color, 1);
 
-//$image->compositeImage($outputImage, imagick::COLOR_ALPHA, 0, 0);
-// If 0 is provided as a width or height parameter,
-// aspect ratio is maintained
-//$image->annotateImage($draw, 10, 45, 0, $show);
+$image->annotateImage($draw, 5, 45, 2, $show);
 //$image->thumbnailImage(100, 0);
 
+$image->thumbnailImage($out_res, 0);
 // I use curl to debug ... when I do that I don't want the image to come back
 if(! preg_match('/curl/', $_SERVER['HTTP_USER_AGENT'])) {
   echo $image;
