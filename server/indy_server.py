d1bed98d8d5d4a6008c7c7a51ee6e1778be82020
Chris McKenzie <kristopolous@yahoo.com>
2015-07-12 21:07:12 -0700

    adding multithreaded

--- server/indy_server.py
+++ server/indy_server.py
@@ -1064,7 +1064,7 @@ def server_manager(config):
     while time.time() - start < patience:
       try:
         print "Listening on %s" % config['port']
-        app.run(port = int(config['port']), host = '0.0.0.0')
+        app.run(threaded = True, port = int(config['port']), host = '0.0.0.0')
         break
 
       except Exception as exc:
