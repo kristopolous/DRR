baf89f21c1e2b5b61fa9d5993f5ee4f083362966
Chris McKenzie <kristopolous@yahoo.com>
2015-07-13 22:00:56 -0700

    partial file support, maybe

--- server/indy_server.py
+++ server/indy_server.py
@@ -935,6 +935,46 @@ def server_manager(config):
       raise RuntimeError('Not running with the Werkzeug Server')
     func()
 
+  # from http://blog.asgaard.co.uk/2012/08/03/http-206-partial-content-for-flask-python
+  @app.after_request
+  def after_request(response):
+    response.headers.add('Accept-Ranges', 'bytes')
+    return response
+
+  def send_file_partial(path):
+    """ 
+      Wrapper around send_file which handles HTTP 206 Partial Content
+      (byte ranges)
+    """
+    range_header = request.headers.get('Range', None)
+    if not range_header: return send_file(path)
+    
+    size = os.path.getsize(path)    
+    byte1, byte2 = 0, None
+    
+    m = re.search('(\d+)-(\d*)', range_header)
+    g = m.groups()
+    
+    if g[0]: byte1 = int(g[0])
+    if g[1]: byte2 = int(g[1])
+
+    length = size - byte1
+    if byte2 is not None:
+      length = byte2 - byte1
+    
+    data = None
+    with open(path, 'rb') as f:
+      f.seek(byte1)
+      data = f.read(length)
+
+    rv = Response(data, 
+      206,
+      mimetype=mimetypes.guess_type(path)[0], 
+      direct_passthrough=True)
+    rv.headers.add('Content-Range', 'bytes {0}-{1}/{2}'.format(byte1, byte1 + length - 1, size))
+
+    return rv
+
   # From http://stackoverflow.com/questions/13317536/get-a-list-of-all-routes-defined-in-the-app
   @app.route("/site-map")
   def site_map():
@@ -951,7 +991,6 @@ def server_manager(config):
 
     return Response('\n'.join(output), mimetype='text/plain')
 
-  
   @app.route('/slices/<path:path>')
   def send_stream(path):
     """
@@ -967,7 +1006,7 @@ def server_manager(config):
     if not os.path.isfile(fname):
       return "File not found. Perhaps the stream is old?", 404
 
-    return flask.send_from_directory(base_dir, path)
+    return send_file_partial("%s/%s" % (base_dir, path))
 
   @app.route('/restart')
   def restart():
