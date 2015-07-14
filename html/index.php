cb03e67939915a231b1ff93133cf64ac689b4abc
Chris McKenzie <kristopolous@yahoo.com>
2015-07-14 13:12:57 -0700

    adding GA

--- html/index.php
+++ html/index.php
@@ -310,16 +310,16 @@ include_once('db.php');
         ev.fire('start');
         ev.fire('name');
 
-        /*
-        ev({
-          station: 'kxlu',
-          name: 'Headspace',
-          day: 'thu',
-          start: '8pm',
-          duration: '2hr'
-        })
-         */
       });
+  // Da Goog!
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-28399789-2', 'auto');
+  ga('send', 'pageview');
+
       </script>
 	</body>
 </html>
