8b2fd647df21240cb3b5dccffbeb6d40e087f32a
Chris McKenzie <kristopolous@yahoo.com>
2015-07-16 18:08:40 -0700

    updating wfmu

--- misc/blob_tool.py
+++ misc/blob_tool.py
@@ -40,23 +40,27 @@ config = config_section_map('Azure', cloud_config)
 container = 'streams'
 blob_service = BlobService(config['storage_account_name'], config['primary_access_key'])
 
+all_files = []
+by_station = {}
 # total storage 
-all_files = [ f for f in blob_service.list_blobs('streams') ]
+for station in ['wfmu']:#'kxlu kpcc kdvs wxyc wcbn wfmu kzsu kvrx'.split(' '):
+   print "Retrieving %s" % station
+   by_station[station] = [ f for f in blob_service.list_blobs('streams', prefix=station, maxresults = 500) ]
+   all_files += by_station[station]
+
+for fname in by_station['wfmu']:
+    print "Deleting ", fname.name
+    blob_service.delete_blob(container, fname.name)
+sys.exit(0)
+
 all_props = [ f.properties for f in all_files ]
 disk_space = [ f.content_length for f in all_props ]
 
 print "total:", sum(disk_space) / (1024.0 ** 3)
+print "files:", len(all_files)
+
 
 # by station
-bucketMap = {}
-for f in all_files:
-  name = f.name
-  station = re.findall('^(\w*)-', name)[0]
-  if station not in bucketMap:
-    bucketMap[station] = {'space': 0, 'count': 0}
-
-  bucketMap[station]['count'] += 1
-  bucketMap[station]['space'] += f.properties.content_length
-
-for station, stats in bucketMap.items():
-  print station, stats['count'], stats['space'] / ( 1024.0 ** 3)
+for station, prop in by_station.items():
+  total_space = sum([ f.properties.content_length for f in prop ])
+  print station, len(prop), total_space / (1024.0 ** 3) 
