5e896d91eda1d67e77fedce080c5f26f07947a83
Chris McKenzie <kristopolous@yahoo.com>
2015-07-16 17:26:57 -0700

    working on blob_tool

--- misc/blob_tool.py
+++ misc/blob_tool.py
@@ -1,4 +1,49 @@
 #!/usr/bin/python -O
 import os
+import sys
+import ConfigParser
 from azure.storage import BlobService
 
+# From https://wiki.python.org/moin/ConfigParserExamples
+def config_section_map(section, Config):
+  """
+  Takes a section in a config file and makes a dictionary
+  out of it.
+
+  Returns that dictionary
+  """
+  dict1 = {}
+  options = Config.options(section)
+
+  for option in options:
+    try:
+      dict1[option] = Config.get(section, option)
+      if dict1[option] == -1:
+        logging.info("skip: %s" % option)
+
+    except Exception as exc:
+      logging.warning("exception on %s!" % option)
+      dict1[option] = None
+
+  return dict1  
+
+cfg = os.environ.get('CLOUD_CFG')
+if cfg is None:
+  print "Define the cloud configuration location with the CLOUD_CFG environment variable"
+  sys.exit(-1)
+
+cloud_config = ConfigParser.ConfigParser()
+cloud_config.read(cfg)
+config = config_section_map('Azure', cloud_config)
+
+container = 'streams'
+blob_service = BlobService(config['storage_account_name'], config['primary_access_key'])
+
+# total storage 
+allprops = [ f.properties for f in blob_service.list_blobs('streams') ]
+disk_space = [ f.properties.content_length for f in allprops ]
+
+print "total: %d" % sum(disk_space) / (1024.0 ** 3)
+
+all_names = [ f.name for f in allprops ]
+print all_names
