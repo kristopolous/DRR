069541b5faa2a96d0067db25cf1dcce9e11e3cf1
Chris McKenzie <kristopolous@yahoo.com>
2015-07-18 15:56:58 -0700

    trying for a more tolerant stitcher

--- server/indy_server.py
+++ server/indy_server.py
@@ -562,14 +562,20 @@ def audio_stitch(file_list, force_stitch=False):
 
     isFound = True
 
+    pos = -1
     try:
-      pos = second['crc32'].index(first['crc32'][-2])
+      while True:
+        pos = second['crc32'].index(first['crc32'][-2], pos + 1)
 
-      for i in xrange(5, 1, -1):
-        if second['crc32'][pos - i + 2] != first['crc32'][-i]:
-          isFound = False
-          logging.warn("Indices do not match between %s and %s" % (first['name'], second['name']))
-          break
+        for i in xrange(5, 1, -1):
+          if second['crc32'][pos - i + 2] != first['crc32'][-i]:
+            isFound = False
+            logging.warn("Indices @%d do not match between %s and %s" % (pos, first['name'], second['name']))
+            continue
+
+        # If we got here it means that everything matches
+        isFound = True
+        break
 
     except Exception as exc:
       logging.warn("Cannot find indices between %s and %s" % (first['name'], second['name']))
