21a9864f75b2739eb325a174a4d7e63e39a29f20
Chris McKenzie <kristopolous@yahoo.com>
2015-07-18 17:59:11 -0700

    registration of a stream

--- server/lib/db.py
+++ server/lib/db.py
@@ -5,79 +5,6 @@ import sqlite3
 g_db = {}
 g_params = {}
 
-def shutdown():
-  global g_db
-
-  for instance in g_db.items():
-    if 'conn' in instance:
-      instance['conn'].close()
-
-def incr(key, value=1):
-  """
-  Increments some key in the database by some value.  It is used
-  to maintain statistical counters.
-  """
-
-  db = connect()
-
-  try:
-    db['c'].execute('insert into kv(value, key) values(?, ?)', (value, key))
-
-  except Exception as exc:
-    db['c'].execute('update kv set value = value + ? where key = ?', (value, key))
-
-  db['conn'].commit()
-
-
-def set(key, value):
-  """ 
-  Sets (or replaces) a given key to a specific value.  
-
-  Returns the value that was sent
-  """
-  global g_params
-
-  db = connect()
-  
-  # From http://stackoverflow.com/questions/418898/sqlite-upsert-not-insert-or-replace
-  res = db['c'].execute('''
-    INSERT OR REPLACE INTO kv (key, value, created_at) 
-      VALUES ( 
-        COALESCE((SELECT key FROM kv WHERE key = ?), ?),
-        ?,
-        current_timestamp 
-    )''', (key, key, value))
-
-  db['conn'].commit()
-
-  g_params[key] = value
-
-  return value
-
-
-def get(key, expiry=0, use_cache=False):
-  """ Retrieves a value from the database, tentative on the expiry """
-  global g_params
-
-  if use_cache and key in g_params:
-    return g_params[key]
-
-  db = connect()
-
-  if expiry > 0:
-    # If we let things expire, we first sweep for it
-    db['c'].execute('delete from kv where key = ? and created_at < (current_timestamp - ?)', (key, expiry))
-    db['conn'].commit()
-
-  res = db['c'].execute('select value, created_at from kv where key = ?', (key, )).fetchone()
-
-  if res:
-    g_params[key] = res[0]
-    return res[0]
-
-  return False
-
-
 def connect():
   """
   A "singleton pattern" or some other fancy $10-world style of maintaining 
@@ -135,6 +62,96 @@ def connect():
 
   return instance
 
+
+def shutdown():
+  global g_db
+
+  for instance in g_db.items():
+    if 'conn' in instance:
+      instance['conn'].close()
+
+def incr(key, value=1):
+  """
+  Increments some key in the database by some value.  It is used
+  to maintain statistical counters.
+  """
+
+  db = connect()
+
+  try:
+    db['c'].execute('insert into kv(value, key) values(?, ?)', (value, key))
+
+  except Exception as exc:
+    db['c'].execute('update kv set value = value + ? where key = ?', (value, key))
+
+  db['conn'].commit()
+
+
+def set(key, value):
+  """ 
+  Sets (or replaces) a given key to a specific value.  
+
+  Returns the value that was sent
+  """
+  global g_params
+
+  db = connect()
+  
+  # From http://stackoverflow.com/questions/418898/sqlite-upsert-not-insert-or-replace
+  res = db['c'].execute('''
+    INSERT OR REPLACE INTO kv (key, value, created_at) 
+      VALUES ( 
+        COALESCE((SELECT key FROM kv WHERE key = ?), ?),
+        ?,
+        current_timestamp 
+    )''', (key, key, value))
+
+  db['conn'].commit()
+
+  g_params[key] = value
+
+  return value
+
+
+def get(key, expiry=0, use_cache=False):
+  """ 
+  Retrieves a value from the database, tentative on the expiry. 
+  If the cache is set to true then it retrieves it from in-memory if available, otherwise
+  it goes out to the db. Other than directly hitting up the g_params parameter which is 
+  used internally, there is no way to invalidate the cache.
+  """
+  global g_params
+
+  if use_cache and key in g_params:
+    return g_params[key]
+
+  db = connect()
+
+  if expiry > 0:
+    # If we let things expire, we first sweep for it
+    db['c'].execute('delete from kv where key = ? and created_at < (current_timestamp - ?)', (key, expiry, ))
+    db['conn'].commit()
+
+  res = db['c'].execute('select value, created_at from kv where key = ?', (key, )).fetchone()
+
+  if res:
+    g_params[key] = res[0]
+    return res[0]
+
+  return False
+
+
+def register_stream(name, start_ts, end_ts):
+  """
+  Registers a stream as existing to be found later when trying to stitch and slice files together.
+  This is all that ought to be needed to know if the streams should attempt to be stitched.
+  """
+  db = connect()
+
+  db['c'].execute('insert into streams(name, start, end) values(?, ?, ?)', name, start_ts, end_ts)
+  db['conn'].commit()
+  return db['c'].lastrowid
+ 
 
 def register_intent(minute_list, duration):
   """
