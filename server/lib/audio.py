fa23b6587f3d5ada1a47bd681749b9c46d47e9c9
Chris McKenzie <kristopolous@yahoo.com>
2015-07-21 13:25:03 -0700

    sorting streams by time

--- server/lib/audio.py
+++ server/lib/audio.py
@@ -113,6 +113,7 @@ def stream_name(list_in, start_minute, duration_minute):
   first_file = list_in[0]['name']
   callsign, unix_time = re.findall('(\w*)-(\d+)', first_file)[0]
 
+  # print unix_time, start_minute
   return "slices/%s-%d_%d.mp3" % (callsign, int(unix_time) + start_minute * 60, duration_minute)
 
 
