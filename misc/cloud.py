44b84400c9636458868eac9868c1b27c8f7af2ff
Chris McKenzie <kristopolous@yahoo.com>
2015-07-23 06:31:39 -0700

    better cloud printout

--- misc/cloud.py
+++ misc/cloud.py
@@ -19,24 +19,27 @@ def get_size(station_list, blob_service):
   by_station = {}
   ix = 1
 
-  for station in station_list:
-    sys.stdout.write( "[ %d / %d ] %s " % (ix, len(station_list), station) )
+  print " Station  Files  Space (GB)"
+  print "----------------------------"
+  for station in sorted(station_list):
+    sys.stdout.write( "%2d: %s " % (len(station_list) - ix + 1, station) )
     sys.stdout.flush()
     by_station[station] = [ f for f in blob_service.list_blobs('streams', prefix=station) ]
-    sys.stdout.write( "... %d\n" % len(by_station[station]) )
+    total_space = sum([ f.properties.content_length for f in by_station[station] ])
+    sys.stdout.write( " %4d %8.3f\n" % (len(by_station[station]),  total_space / (1024.0 ** 3)) )
     all_files += by_station[station]
     ix += 1
 
   all_props = [ f.properties for f in all_files ]
   disk_space = [ f.content_length for f in all_props ]
 
-  print "total:", sum(disk_space) / (1024.0 ** 3)
-  print "files:", len(all_files)
+  gb = sum(disk_space) / (1024.0 ** 3)
+  print "----------------------------"
+  print " %-8s %d %8.3f GB" % ("Total", len(all_files), gb)
+  print " %-7s  $%.02f/month" % ("Cost", gb * 0.024)
+  print
+  print " *using 0.024c per GB azure pricing"
 
-  # by station
-  for station, prop in by_station.items():
-    total_space = sum([ f.properties.content_length for f in prop ])
-    print station, len(prop), total_space / (1024.0 ** 3) 
 
 cfg = os.environ.get('CLOUD_CFG')
 
