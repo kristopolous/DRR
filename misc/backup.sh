5f38f19021bbd3750146c0130015d029b04adebe
Chris McKenzie <kristopolous@yahoo.com>
2015-07-23 17:46:52 -0700

    backing up on a per-directory time-slice basis

--- misc/backup.sh
+++ misc/backup.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-# Decide on a directory
-backup_dir=~/backups/indycast
-[ -e $backup_dir ] || mkdir -p $backup_dir
-
 # Make a base file name
 fname_base=`date +"%Y%m%d-%H%M"`
 
+# Decide on a directory
+backup_dir=~/backups/indycast/$fname_base
+[ -e $backup_dir ] || mkdir -p $backup_dir
+
 # First we list all of the stations
 for station in `./server_query.py -n -l`; do
-  echo "Backing up $station"
-  ./server_query.py -q db -c $station > $backup_dir/$station-$fname_base.gz
+  echo -n "Backing up $station ... "
+  ./server_query.py -q db -c $station > $backup_dir/$station.gz
+  echo `du -b $backup_dir/$station.gz | awk ' { print $1 } '`
 done
+du -k $backup_dir
