af9619dd05e08dcbccc04bfb45f28feb66787cba
Chris McKenzie <kristopolous@yahoo.com>
2015-07-24 14:47:51 -0700

    Making backup scriptable

--- misc/backup.sh
+++ misc/backup.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+#
+# Downloads gzipped sqlite3 databases from all the stations
+# specified in the configuration directory (from server-query)
+#
+# If an argument is supplied then it just echos the backup directory
+# This is useful for scripting this in larger infrastructures
+#
+
+scripted=$#
 
 # Make a base file name
 fname_base=`date +"%Y%m%d-%H%M"`
@@ -9,8 +18,13 @@ backup_dir=~/backups/indycast/$fname_base
 
 # First we list all of the stations
 for station in `./server_query.py -n -l`; do
-  echo -n "Backing up $station ... "
+
+  [ $scripted ] || echo -n "Backing up $station ... "
+
   ./server_query.py -q db -c $station > $backup_dir/$station.gz
-  echo `du -b $backup_dir/$station.gz | awk ' { print $1 } '`
+
+  [ $scripted ] || echo `du -b $backup_dir/$station.gz | awk ' { print $1 } '`
+
 done
-du -k $backup_dir
+
+[ $scripted ] && echo $backup_dir || du -k $backup_dir
