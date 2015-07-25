53f8b548c3fdf5706804b54825d7a36cb460fb1a
Chris McKenzie <kristopolous@yahoo.com>
2015-07-24 22:24:43 -0700

    plist needs to be a little better

--- server/indy_server.py
+++ server/indy_server.py
@@ -453,7 +453,7 @@ def server_manager(config):
       'uptime': int(time.time() - misc.start_time),
       'free': os.popen("df -h / | tail -1").read().strip(),
       'load': os.popen("uptime").read().strip(),
-      'plist': os.popen("ps auf | grep %s" % misc.config['callsign']).read().strip().split('\n'),
+      'plist': os.popen("ps auxf | grep %s" % misc.config['callsign']).read().strip().split('\n'),
       'disk': sum(os.path.getsize(f) for f in os.listdir('.') if os.path.isfile(f)),
       'streams': DB.all('streams', sort_by='start_unix'),
       'version': __version__,
