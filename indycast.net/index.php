1bc78599a7b60b0d49df2f9b59632328eea467b9
Chris McKenzie <kristopolous@yahoo.com>
2015-07-25 22:55:31 +0000

    removing unneded scripts

--- indycast.net/index.php
+++ indycast.net/index.php
@@ -189,11 +189,11 @@ if(isset($_GET['callsign'])) {
 
     <script src='/assets/js/underscore-min.js'></script>
     <script src="/assets/js/jquery.min.js"></script>
-    <script src="/assets/js/skel.min.js"></script>
-    <script src="/assets/js/util.min.js"></script>
+<!--    <script src="/assets/js/skel.min.js"></script>
+    <script src="/assets/js/util.min.js"></script> -->
     <script src="/assets/js/evda.min.js"></script>
     <!--[if lte IE 8]><script src="/assets/js/ie/respond.min.js"></script><![endif]-->
-    <script src="/assets/js/main.min.js"></script>
+<!--    <script src="/assets/js/main.min.js"></script>-->
     <script src="/assets/js/indycast.js"></script>
     <? if ($callsign) { ?>
       <script> ev('station', '<?= $callsign ?>'); </script>
