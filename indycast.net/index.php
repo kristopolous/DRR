c3094fe1c5cb136975abf591d9cfb766af1fdbc7
Chris McKenzie <kristopolous@yahoo.com>
2015-07-25 13:01:06 -0700

    cleaning up css

--- indycast.net/index.php
+++ indycast.net/index.php
@@ -149,7 +149,7 @@ if(isset($_GET['callsign'])) {
             <form action="https://www.paypal.com/cgi-bin/webscr" method="post" id="donate">
               <input type="hidden" name="cmd" value="_s-xclick">
               <input type="hidden" name="hosted_button_id" value="X4J4BD86VTXWS">
-              <input class="button" type="submit" value="Donate via PayPal" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_SM.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+              <input class="button fit" type="submit" value="Donate via PayPal" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_SM.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
               <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
             </form>
           </div>
