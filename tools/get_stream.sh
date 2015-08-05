255e3407ae26c48dec480fca00b649784383d317
Chris McKenzie <kristopolous@yahoo.com>
2015-08-05 16:50:12 -0700

    Ability to get multiple streams at once

--- tools/get_stream.sh
+++ tools/get_stream.sh
@@ -2,7 +2,11 @@
 # Gets a remote stream (presuming you have the right credentials)
 # based on a file name
 
-stream=`basename $1`
-station=$(echo $stream | sed 's/-.*//')
+while [ $# -gt 0 ]; do
+  stream=`basename $1`
+  station=$(echo $stream | sed 's/-.*//')
 
-[ -e $stream ] || scp $station.indycast.net:radio/$station/streams/$stream .
+  [ -e $stream ] || scp $station.indycast.net:radio/$station/streams/$stream .
+
+  shift
+done
