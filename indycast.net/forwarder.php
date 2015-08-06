f61ab208d863424a1dab69f09b425ff774fffece
Chris McKenzie <kristopolous@yahoo.com>
2015-08-06 00:02:54 +0000

    not 302'ing xml for real now

--- indycast.net/forwarder.php
+++ indycast.net/forwarder.php
@@ -8,14 +8,20 @@ $station = get_station(['callsign' => $callsign]);
 if($station) {
   // Don't redirect unless needed
   $url = 'http://' . $station['base_url'] . '/' . implode("/", array_map("rawurlencode", explode("/", $request)));
-  if (array_search($request, ['my_uuid', 'heartbeat', 'help', 'stats', '.xml']) !== false) {
+
+  if (
+    strpos($request, '.xml') !== false || 
+    array_search($request, ['my_uuid', 'heartbeat', 'help', 'stats']) !== false
+  ) {
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
     $data = curl_exec($ch);
     $info = curl_getinfo($ch);
     header('Content-Type: ' . $info['content_type']);
+
     echo $data;
+
   } else {
     header('Location: ' . $url);
   }
