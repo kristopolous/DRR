530c81c0c5f4b70958705c19d2308bbf73f72268
Chris McKenzie <kristopolous@yahoo.com>
2015-08-07 16:30:36 -0700

    more refactoring

--- indycast.net/reminder.php
+++ indycast.net/reminder.php
@@ -69,7 +69,14 @@ label { font-size: 0.8em}
   <input type='text' name='notes'>
   <button>Send me a reminder</button>
 </form>
-<h2>About</h2>
+    </div>
+    <div id="footer">
+      <div class="container 75%">
+
+        <header class="major last">
+          <h2>About</h2>
+        </header>
+        <div style="text-align: left">
 <p>Listening to something right now but have to run and don't have the time to finish it?</p>
 <p>Miss the beginning of something and want to catch it later?</p>
 <h3>We'll send you a reminder with a link to the audio. For free of course.</h3>
@@ -77,7 +84,7 @@ label { font-size: 0.8em}
 <p>You can even leave notes for your future-self telling yourself why you think it's so awesome.</p>
 <p>Later on, when the show is over, an email will be sent to you with a link and the notes you leave.</p>
 
-<p><b>Privay policy:</b> We don't collect email addresses and we delete everything from our database after we send the email off to you.  Don't worry, we're on your side!</p>
+<p><b>Privacy policy:</b> We don't collect email addresses and we delete everything from our database after we send the email off to you.  Don't worry, we're on your side!</p>
 
 
 <script>
