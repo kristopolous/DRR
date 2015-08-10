3becc9975d0179903624b948cd99da38a255a687
Chris McKenzie <kristopolous@yahoo.com>
2015-08-09 18:53:24 -0700

    request_job reading configs, moved to tools

--- tools/request_job.py
+++ tools/request_job.py
@@ -1,5 +1,22 @@
 #!/usr/bin/python -O
 import re
+import requests
+import ConfigParser
+import lib.misc as misc
+
+# Taken from https://bradgignac.com/2014/05/12/sending-email-with-python-and-the-mailgun-api.html
+def send_email():
+  key = 'YOUR API KEY HERE'
+  recipient = 'YOUR EMAIL HERE'
+
+  request_url = 'https://api.mailgun.net/v3/indycast.net'
+  request = requests.post(request_url, auth=('api', key), data={
+    'from': 'hello@example.com',
+    'to': recipient,
+    'subject': 'Hello',
+    'text': 'Hello from Mailgun'
+  })
+
 
 def do_template(settings):
 
