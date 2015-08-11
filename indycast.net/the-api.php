fb8a26a2152ea20d0f048a7685ed7adbe3c1018c
Chris McKenzie <kristopolous@yahoo.com>
2015-08-11 16:12:36 -0700

    api for reminders

--- indycast.net/the-api.php
+++ indycast.net/the-api.php
@@ -1,10 +1,29 @@
 <?php
 include_once('common.php');
 
-$request = $_GET['func'];
+function pl_reminder($what) {
+  $param_map = sanitize(
+    ['start_time', 'end_time', 'email', 'notes', 'callsign']
+  );
 
-if($request == 'stations') {
-  echo json_encode(active_stations());
+  $lhs = array_keys($dirty); $rhs = array_values($dirty);
+  if(array_search(false, $rhs, true) !== false) {
+    return false;
+  }
+
+  return $db->exec('insert into reminders (' . implode(',', $lhs) . ') values ("' . implode('","', $rhs) . '")');
+}
+
+function pl_stations() {
+  return json_encode(active_stations());
+}
+
+if(isset($_REQUEST['func']) && function_exists('pl_' . $_REQUEST['func'])) {
+  $toRun = 'pl_' . $_REQUEST['func'];
+  unset($_REQUEST['func']);
+
+  $result = $toRun ( $_REQUEST );
+  echo $result;
 } else {
   echo json_encode(['error' => 'uknown api call']);
 }
