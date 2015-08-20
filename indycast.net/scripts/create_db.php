139fc753741752c35d5d8ccf3d6d2bad0234c174
Chris McKenzie <kristopolous@yahoo.com>
2015-08-20 16:38:30 -0700

    Fixing a syntax issue with the upgrade strategy

--- indycast.net/scripts/create_db.php
+++ indycast.net/scripts/create_db.php
@@ -12,28 +12,29 @@ function get_column_list($table_name) {
   }, sql_all($res));
 }
 
-foreach($schema as $table_name => $tbl_schema) {
+foreach($schema as $table_name => $table_schema) {
   $existing_column_name_list = get_column_list($table_name);
 
   // This means we need to create the table
   if (count($existing_column_name_list) == 0) {
+    echo "Creating the $table_name table.";
 
-    $schema = implode (',', sql_kv($tbl_schema, '', ''));
+    $schema = implode (',', sql_kv($table_schema, '', ''));
     $db->exec("create table $table_name ( $schema )");
 
   } else {
 
     // Otherwise we may need to add columns to the table
-    $our_column_name_list = array_keys($tbl_schema);
+    $our_column_name_list = array_keys($table_schema);
 
     $column_to_add_list = array_diff($our_column_name_list, $existing_column_name_list);
 
     if(count($column_to_add_list)) {
-      echo "Adding the following columns from $tbl_name:";
-      print_r($column_to_add_list);
+      echo "Adding the following columns from $table_name:";
+      echo "  " . implode(', ', $column_to_add_list);
 
       foreach($column_to_add_list as $column_to_add) {
-        $column_to_add_schema = $tbl_schema[$column_to_add];
+        $column_to_add_schema = $table_schema[$column_to_add];
         $db->exec("alter table $table_name add column $column_to_add $column_to_add_schema");
       }
 
@@ -44,10 +45,10 @@ foreach($schema as $table_name => $tbl_schema) {
 
     // See if we need to remove any columns
     if (count($column_to_remove_list) > 0) {
-      echo "Removing the following columns from $tbl_name:";
-      print_r($column_to_remove_list);
+      echo "Removing the following columns from $table_name:";
+      echo "  " . implode(', ', $column_to_remove_list);
 
-      $our_schema = implode(',', sql_kv($tbl_schema, '', ''));
+      $our_schema = implode(',', sql_kv($table_schema, '', ''));
       $our_columns = implode(',', $our_column_name_list);
 
       $drop_column_sql = "
