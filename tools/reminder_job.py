0ff33a3571813b63130c75c39cabfb9124d58aea
Chris McKenzie <kristopolous@yahoo.com>
2015-08-20 23:16:42 -0700

    Making the reminder use the new format

--- tools/reminder_job.py
+++ tools/reminder_job.py
@@ -9,16 +9,19 @@ import lib.db as DB
 import time
 import sys
 
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
+
 def find_requests_and_send_mail(config):
-  db = DB.connect('../db/main.db')
+  db = DB.connect(db_file='../db/main.db')
   now = time.time()
   what_we_be_done_with = db['c'].execute('select * from reminders where end_time < %d' % now).fetchall()
 
   for row in DB.map(what_we_be_done_with, 'reminders'):
-    row['link'] = "http://indycast.net/%s/slices/%s-%s_%d.mp3" % ( row['station'], row['station'], time.strftime("%Y%m%d%H%M", time.gmtime(row['start_time'] - row['offset'] * 60)), (row['end_time'] - row['start_time']) / 60)
+    row['link'] = "http://indycast.net/%s/slices/%s_%d" % ( row['station'], time.strftime("%Y%m%d%H%M", time.gmtime(row['start_time'] - row['offset'] * 60)), (row['end_time'] - row['start_time']) / 60)
+    row['link'] += '/%s_on_%s.mp3' % (re.sub(' ', '_', row['notes']), row['station'])
 
     email = do_template(template_file='email_reminder_template.txt', settings=row)
-    res = misc.send_email(config=config, who='kristopolous@yahoo.com', subject=email['subject'], body=email['body'])
+    res = misc.send_email(config=config, who=row['email'], subject=email['subject'], body=email['body'])
     db['c'].execute('delete from reminders where id = %d' % row['id'])
     db['conn'].commit()
 
