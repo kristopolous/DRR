367316bb07cc5df95cd815fd80a26b6e917fdd97
Chris McKenzie <kristopolous@yahoo.com>
2015-08-22 16:55:08 -0700

    reformatting

--- indycast-2.net/template.php
+++ indycast-2.net/template.php
@@ -1,37 +1,50 @@
 <?
 include_once('../indycast.net/common.php');
 ?>
-<!DOCTYPE html>
-<html lang="en">
-
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
 <head>
-
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>Indycast - A technology to record the radio that doesn't get podcasted</title>
-
-    <!-- Bootstrap Core CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom CSS -->
-    <link href="css/sb-admin.css" rel="stylesheet">
-
-    <!-- Custom Fonts -->
-    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <meta http-equiv="Content-Style-Type" content="text/css" />
+  <style type="text/css">code{white-space: pre;}</style>
+  <link href='http://fonts.googleapis.com/css?family=Inconsolata' rel='stylesheet' type='text/css'>
+  <link href='http://fonts.googleapis.com/css?family=Lora' rel='stylesheet' type='text/css'>
+  <link href='http://fonts.googleapis.com/css?family=Arvo' rel='stylesheet' type='text/css'>
+  <link href='http://fonts.googleapis.com/css?family=Slabo+27px' rel='stylesheet' type='text/css'>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta charset="utf-8" />
+  <meta name="description" content="indycast: community driven podcasting" />
+  <meta property="og:site_name" content="Indycast" />
+  <meta property="og:url" content="http://indycast.net" />
+  <meta property="og:title" content="About indycast, community driven podcasting" />
+  <meta property="og:type" content="website" />
+  <meta property="og:description" content="Welcoming lazy hackers from the world over." />
+  <meta property="og:image" content="http://indycast.net/og-image.php" />
+  <meta name="twitter:card" content="summary_large_image" />
+  <meta name="twitter:site" content="@indycaster" />
+  <meta name="twitter:creator" content="@indycaster" />
+  <meta name="twitter:title" content="incycast: community drivne podcasting" />
+  <meta name="twitter:url" content="http://indycast.net" />
+  <meta name="twitter:description" content="Welcoming lazy hackers from the world over." />
+  <meta name="twitter:image:src" content="http://indycast.net/images/twit-image.jpg" />
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" href="favicon.ico" >
+  <title>Indycast - A technology to record the radio that doesn't get podcasted</title>
+  <link href="/css/bootstrap.min.css" rel="stylesheet">
+  <link href="/css/sb-admin.css" rel="stylesheet">
+  <link href="/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+  <![endif]-->
 </head>
-
 <body>
 
     <div id="wrapper">
