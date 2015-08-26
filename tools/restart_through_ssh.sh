66be5b33c43c645637e6b8c8fe290974972bd0ed
Chris McKenzie <kristopolous@yahoo.com>
2015-08-25 19:20:57 -0700

    updating comments

--- tools/restart_through_ssh.sh
+++ tools/restart_through_ssh.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+# 
+# Restarts a single server through ssh under the following assumptions:
+#
+#   * The station exists 
+#   * Is registered through indycast.net
+#   * Public keys will work
+#   * The server code is in DRR/server
+#   
 station=$1
 
 ssh $station.indycast.net "cd DRR/server;pkill $station;./indy_server.py -c configs/$station.txt --daemon &" &
