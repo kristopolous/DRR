20d5ce71c3f06a142bdeb1b31e039c8c00b07a8f
Chris McKenzie <kristopolous@yahoo.com>
2015-08-27 18:02:14 -0700

    using old non-bootstrapped page

--- indycast-2.net/content/index.php
+++ indycast-2.net/content/index.php
@@ -1,114 +1,157 @@
 <?
 include_once('../indycast.net/common.php');
 ?>
+  <link rel="stylesheet" href="/assets/css/main.css" />
 <title>Indycast - A technology to record the radio that doesn't get podcasted</title>
-<div class="row">
-  <div class="col-lg-12">
-    <h1 class="page-header">
-      A DVR for Radio <small>easy, free, open source.</small>
-    </h1>
-  </div>
-</div>
+<!--
+      <header class="major container 75%">
+        <h2>
+        Subscribe to any broadcast
+        <br />
+        Just by knowing when it's on.
+        </h2>
+      </header>
+-->
+    <div id="header">
+      <h1>Indycast DVR</h1>
+      <p>A free <a href="/about.html">technology</a> to record the <?= $callsign ? strtoupper($callsign) : "radio that doesn't get podcasted" ?>
+      <?php if ($callsign) { ?><br/><small>(<a href="/">and more</a>)</small><?php } ?></p>
+      <small>Works on your <?= $device ?>. No signup or app needed.</small>
+    </div>
+      <div class="box alt container"><?php 
+      if(!$callsign) { ?>
+        <section class="feature left">
+          <a href="#" class="image icon fa-signal"><img src="/images/pic01.jpg" alt="" /><div id="description"></div></a>
+          <div class="content">
+            <h3>Choose the station</h3>
+            <div id='station-search-box'>
+              <i class="fa fa-search"></i>
+              <input type="text" placeholder="Search" id='station-query'>
+            </div>
+            <ul class="radio-group group" id="station"><?php
+              foreach(active_stations() as $station) {
+                echo '<li><a desc="' . $station['description'] . '" class="button">' . ($station['callsign']) . '</a></li>';
+              }
+            ?></ul>
+            <a href="#volunteer">Volunteer to add a station!</a>
+          </div>
+        </section>
+        <?php } ?>
+        <section class="feature right">
+          <a href="#" class="image icon fa-clock-o"><img src="/images/pic02.jpg" alt="" /></a>
+          <div class="content" id='day-picker'>
 
-<div class="row">
-  <div class="col-lg-4">
-    <div class="panel panel-default">
-      <div class="panel-heading">
-        <h3 class="panel-title">1. Pick a Station</h3>
+            <label for="day">day of week to record on</label>
+            <ul class="week-group" id="day">
+              <li><a class="button">sun</a></li>
+              <li><a class="button">mon</a></li>
+              <li><a class="button">tue</a></li>
+              <li><a class="button">wed</a></li>
+              <li><a class="button">thu</a></li>
+              <li><a class="button">fri</a></li>
+              <li><a class="button">sat</a></li>
+            </ul>
+            <div id='time'>
+              <label for="start">Starting at</label>
+              <div id='time-controls'>
+                <input class="text" size=4 type="text" name="start" id="start" value="" placeholder="ex: 3:30 PM" /><ul class="week-group group inline" id="ampm">
+                  <li><a class="button">am</a></li>
+                  <li><a class="button">pm</a></li>
+                </ul>
+              </div>
+
+            </div>
+            <label for="duration">For how long</label>
+            <ul class="week-group group" id="duration">
+              <li><a data="30" class="button">30 min</a></li>
+              <li><a data="1hr" class="button">1 hr</a></li>
+              <li><a data="1hr30" class="button">1&frac12; hrs</a></li>
+              <li><a data="2hr" class="button">2 hrs</a></li>
+            </ul>
+          </div>
+        </section>
+        <section class="feature left">
+          <a href="#" class="image icon fa-mobile"><img src="/images/pic03.jpg" alt="" /></a>
+          <div class="content">
+            <h3>Give it a name</h3>
+            <input class="text" type="text" name="name" id="name" value="" placeholder="ex. Alien Air Music" />
+          </div>
+        </section>
       </div>
-      <div class="panel-body">
-        <div class="list-group"><?
-          foreach(active_stations() as $station) {
-            echo '<a desc="' . $station['description'] . '" class="list-group-item button">' . ($station['callsign']) . '</a>';
-          }?>
+      <footer class="major container">
+        <div id="podcast-done">
+          <h3>My free subscription</h3>
+          <a id="podcast-url" class='big-button disabled'></a>
         </div>
-        <div class="text-right">
-          <a href="#">Volunteer to Add a Station <i class="fa fa-arrow-circle-right"></i></a>
+      </footer>
+
+    </div>
+
+    <div id="footer">
+      <div class="container 75%">
+
+        <header class="major last">
+          <h2>About</h2>
+        </header>
+        <div style="text-align: left">
+          <p>Due to their limited resources, much of independent radio is not syndicated or available after broadcast.  We want to enjoy the benefits of time-shifted programming on these stations.</p>
+
+          <a name="volunteer"></a><h3>How it works</h3>
+          <p>The web stream of the station is recorded and then saved and delivered for personal use.  Every station is a different server which acts in a federation.</p>
+
+          <h3>Who runs this</h3>
+          <p>People like you. Voluntarily.</p>
+
+          <h3>Join the Federation</h3>
+          <p>Generally each station gets its own server. For instance, kxlu.indycast.net and kdvs.indycast.net are different servers responsible for each station.</p>
+
+          <p><a href='https://github.com/kristopolous/DRR/wiki/Join-the-Federation'>If you'd like to add or support a station, join the federation</a>.</p>
+
+          <p>We also accept <a href=https://github.com/kristopolous/DRR/wiki/How-To-Donate>donations of VPS nodes</a> and money. Thanks for supporting indy radio in the 21st century.
+          <div class="active-list">
+            <a href='https://github.com/kristopolous/DRR/wiki/Join-the-Federation' class='button'>Join The Federation</a> <span id='or'>or</span> 
+            <a name="donate"></a>
+<form action="https://www.paypal.com/cgi-bin/webscr" method="post" id="donate"><input type="hidden" name="cmd" value="_s-xclick"><input type="hidden" name="hosted_button_id" value="X4J4BD86VTXWS"><input class="button fit" type="submit" value="Donate via PayPal" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_SM.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!"><img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1"></form>
+          </div>
         </div>
+
+        <header class="major last">
+          <h2>Questions or comments</h2>
+        </header>
+
+        <p>For questions, comments, or to report a device that isn't supported, please <a href="mailto:indycast@googlegroups.com">send an email</a> to <a href="https://groups.google.com/d/forum/indycast">the mailing list</a>. Thanks!</p>
+
+        <ul class="icons">
+          <li><a href="https://twitter.com/indycaster" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+          <li><a href="http://github.com/kristopolous/DRR/" class="icon fa-github"><span class="label">Github</span></a></li>
+        </ul>
+
+        <ul class="copyright">
+          <li>This is an <a href="https://github.com/kristopolous/DRR">open source project</a>.</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+        </ul>
+
       </div>
     </div>
-  </div>
-  <div class="col-lg-8">
-    <div class="panel panel-default">
-      <div class="panel-heading">
-        <h3 class="panel-title">Step 2. Time to Record</h3>
-      </div>
-      <div class="panel-body">
-        <div class="content" id='day-picker'>
-
-         <div class="form-group">
-            <label for="day">Day of week to record on</label><br/>
-            <div class="btn-group week-group" id="day" role="group">
-              <button type="button" class="btn btn-default">sun</button>
-              <button type="button" class="btn btn-default">mon</button>
-              <button type="button" class="btn btn-default">tue</button>
-              <button type="button" class="btn btn-default">wed</button>
-              <button type="button" class="btn btn-default">thu</button>
-              <button type="button" class="btn btn-default">fri</button>
-              <button type="button" class="btn btn-default">sat</button>
-            </div>
-          </div>
-
-          <div id='time' class='form-group'>
-            <label for="start">Starting at</label>
-            <div id='time-controls'>
-              <div class="input-group input-group-lg inline">
-                <input class="text form-control" size="4" type="text" name="start" id="start" value="" placeholder="ex: 3:30 PM" />
-                <div class="btn-group week-group inline" id="ampm" role="group">
-                  <button type="button" class="btn btn-default">am</button>
-                  <button type="button" class="btn btn-default">pm</button>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="form-group">
-            <label for="duration">For how long</label><br/>
-            <div class="week-group btn-group" id="duration">
-              <button data="30" type="button" class="btn btn-default">30</button>
-              <button data="1hr" type="button" class="btn btn-default">1hr</button>
-              <button data="1hr30" type="button" class="btn btn-default">1&frac12; hrs</button>
-              <button data="2hr" type="button" class="btn btn-default">2hr</button>
-            </div>
-          </div>
+    <script type='text/template' id='tpl-podcast'>
+      <span id='rss-top'>
+        <div id='rss-img'>
+          <img src='/images/rss_64.png'>
         </div>
+        <div id='rss-header'>
+          <h3 id='rss-title'><%= name %></h3>
+          <span id='rss-time'><% if (phrase) { %><%= phrase %><% } else { %><%= day %> at <%= time %> on <%= station %><% } %></span>
+        </div>
+      </span>
+      <span id='podcast-link'>
+        <%= parts.join(' <br> ') %>
+      </span>
+    </script>
 
-        <div class="form-group content">
-          <label for='name'>Give it a name</label>
-          <div class="input-group input-group-lg">
-            <input class="text form-control" type="text" name="name" id="name" value="" placeholder="ex. Alien Air Music" />
-          </div>
-        </div>
-     </div>
-    </div>
-    <div class="panel panel-default">
-      <div class="panel-heading">
-        <h3 class="panel-title">Step 3. You're Done</h3>
-      </div>
-      <div class="panel-body">
-        <h3>My free subscription</h3>
-        <a id="podcast-url" class='big-button disabled'></a>
-        <div class="panel panel-green">
-          <div class="panel-heading">
-            <div class="row">
-              <div class="col-xs-3">
-                <i class="fa fa-tasks fa-5x"></i>
-              </div>
-              <div class="col-xs-9 text-right">
-                <div class="huge">12</div>
-                <div>New Tasks!</div>
-              </div>
-            </div>
-          </div>
-          <a href="#">
-            <div class="panel-footer">
-              <span class="pull-left">View Details</span>
-              <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
-              <div class="clearfix"></div>
-            </div>
-          </a>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
+    <script src='//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min.js'></script>
+    <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
+    <script src="/assets/js/evda.min.js"></script>
+    <!--[if lte IE 8]><script src="/assets/js/ie/respond.min.js"></script><![endif]-->
+    <script src="/assets/js/indycast.js"></script>
+    <?php if ($callsign) { ?>
+      <script> ev('station', '<?= $callsign ?>'); </script>
+    <?php } ?>
