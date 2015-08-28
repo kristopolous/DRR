6204ebffa013a8d11847d83a2991e7389b7db1ef
Chris McKenzie <kristopolous@yahoo.com>
2015-08-27 18:01:04 -0700

    stub for the live radio

---
+++ indycast-2.net/content/live.php
@@ -0,0 +1,9 @@
+Listen to your station live or 
+
+Pick your station
+How many minutes ago do you want to start from?
+
+Pick a specific time
+
+You can listen to it using the player below or
+you can click here for an external player
