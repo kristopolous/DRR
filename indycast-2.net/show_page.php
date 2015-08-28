55cd07616d45c5791bae66d6ef9d546d1771e2bc
Chris McKenzie <kristopolous@yahoo.com>
2015-08-27 22:09:00 -0700

    more fixes

--- indycast-2.net/show_page.php
+++ indycast-2.net/show_page.php
@@ -59,7 +59,7 @@ if(isset($_GET['callsign'])) {
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="icon" href="favicon.ico" >
   <link href="/css/bootstrap.min.css" rel="stylesheet">
-  <link href="/css/sb-admin.css" rel="stylesheet">
+  <link href="/css/sb-admin.css" rel="stylesheet"> 
   <link href="/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
