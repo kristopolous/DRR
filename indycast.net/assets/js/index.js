313311e4b254c4dbd1f985e0131a4d176addd7a9
Chris McKenzie <kristopolous@yahoo.com>
2015-09-03 14:34:15 -0700

    js refactoring

--- indycast.net/assets/js/index.js
+++ indycast.net/assets/js/index.js
@@ -1,19 +1,3 @@
-
-var 
-  // #57 - see why ipad needs to double click
-  isiDevice = navigator.userAgent.match(/ip(hone|od|ad)/i),
-  isMobile = true,
-  page = false,
-  listenEvent = isiDevice ? 'touchend' : 'click',
-  ev = EvDa({start: '', name: '', day: []}),
-  fullName = {
-    sun: 'Sundays', mon: 'Mondays', tue: 'Tuesdays', wed: 'Wednesdays',
-    thu: 'Thursdays', fri: 'Fridays', sat: 'Saturdays'
-  },
-  tpl = {},
-  time_re = /^\s*(1[0-2]|[1-9])(:[0-5][0-9])?\s*([ap]m)?\s*$/i;
-
-
 ev('ampm', function(val){
   if(val) {
     $("#start").val($("#start").val().replace(/\s*([ap]m)\s*/i, val))
@@ -146,17 +130,6 @@ ev.test('start', function(v, cb, meta) {
   cb(res);
 });
 
-function htmldo(what) {
-  // hyperlinking
-  what = what.replace(/[a-z]+:\/\/[^\s^<]+/g, '<a href="$&" target=_blank>$&</a>');
-
-  // paragrapherizing
-  what = what.replace(/\n/g, '</p><p>');
-
-  // finalerizing
-  return '<p>' + what + '</p>';
-}
-
 $(function() {
 
   tpl.podcast = _.template($("#tpl-podcast").html());
@@ -241,16 +214,3 @@ $(function() {
 
 });
 
-function station_select() {
-  $("#station-preselect").slideUp();
-  $("#station").slideDown();
-}
-
-// #30: Da Goog!
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-ga('create', 'UA-28399789-2', 'auto');
-ga('send', 'pageview');
