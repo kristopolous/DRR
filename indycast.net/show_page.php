21748ff698c06b3a19c8a4c2d01fbf5b301931ea
Chris McKenzie <kristopolous@yahoo.com>
2015-09-03 14:58:53 -0700

    consolidating locations

--- indycast.net/show_page.php
+++ indycast.net/show_page.php
@@ -63,8 +63,8 @@ if(isset($_GET['callsign'])) {
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <script src="//oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="//oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
   <![endif]-->
 </head>
 <body>
@@ -94,7 +94,6 @@ if(isset($_GET['callsign'])) {
 
     <div class="container-fluid page-content">
 <? 
-
 $emit_script = "
   <script src='/assets/js/evda.min.js'></script>
   <script src='/assets/js/common.js'></script>
