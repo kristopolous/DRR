d68c355af232763b890a133facef2336ec1a47fb
Chris McKenzie <kristopolous@yahoo.com>
2015-09-12 14:36:47 -0700

    dl buttons for mobile

--- indycast.net/content/index.php
+++ indycast.net/content/index.php
@@ -196,7 +196,7 @@
 
     <div class='well'>
       <h5>Get <%= showname %> Weekly for free!</h5>
-      <div style='text-align: center'>
+      <div id='dl-buttons'>
         <a target=_blank href='<%= podcast_url %>' class='btn btn-lg btn-default'><i class="fa fa-apple"></i> In iTunes</a>
         <a target=_blank href='<%= podcast_url %>' class='btn btn-lg btn-default'><i class="fa fa-rss-square"></i> In another podcaster</a>
         <button data-toggle="modal" data-target="#dialog-email" type='button' class='btn btn-lg btn-default'><i class="fa fa-envelope"></i> Emailed to me</button>
