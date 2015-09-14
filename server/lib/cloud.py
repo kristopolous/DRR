ca9753e1968bf53f186277c381ec5a822009a852
Chris McKenzie <kristopolous@yahoo.com>
2015-09-14 02:35:20 +0000

    GREAT! FAN FUCKING TASTIC! FUCKING FINALLY

--- server/lib/cloud.py
+++ server/lib/cloud.py
@@ -452,7 +452,9 @@ def prune_process(lockMap, reindex=False):
   # cloud thingie associated with it.
   db = DB.connect()
 
-  unlink_list = db['c'].execute('select name, id from streams where end_unix < date("now", "-%d seconds") or (end_minute - start_minute < 0.05 and end_unix < date("now", "-%d seconds"))' % (archive_duration, 1200)).fetchall()
+  logging.info('select name, id from streams where end_unix < date("now", "-%d seconds") or (end_minute - start_minute < 0.05 and start_unix < date("now", "%d seconds"))' % (archive_duration, TS.get_offset() * 60 - 1200))
+
+  unlink_list = db['c'].execute('select name, id from streams where end_unix < date("now", "-%d seconds")' % (archive_duration)).fetchall()
 
   for file_name_tuple in unlink_list:
     file_name = str(file_name_tuple[0])
