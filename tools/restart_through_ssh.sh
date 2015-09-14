a9aec78c7ce476094b75b005775a3389a5910086
Chris McKenzie <kristopolous@yahoo.com>
2015-09-13 20:36:33 -0700

    restart does upgrade

--- tools/restart_through_ssh.sh
+++ tools/restart_through_ssh.sh
@@ -9,4 +9,4 @@
 #   
 station=$1
 
-ssh $station.indycast.net "cd DRR/server;pkill $station;./indy_server.py -c configs/$station.txt --daemon &" &
+ssh $station.indycast.net "cd DRR;git pull;./bootstrap.sh;cd server;pkill $station;./indy_server.py -c configs/$station.txt --daemon &" &
