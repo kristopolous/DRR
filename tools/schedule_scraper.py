20702cf065fcf998f9b7a12fb60cb63d3ed9fd33
Chris McKenzie <kristopolous@yahoo.com>
2015-09-21 16:02:04 -0700

    updating scraper

--- tools/schedule_scraper.py
+++ tools/schedule_scraper.py
@@ -4,6 +4,23 @@
 #
 import urllib2
 import json
+import pycurl
 
-active_stations_raw = urllib2.urlopen("http://indycast.net/api/stations").read()
-active_station = json.loads(active_stations_raw)
+def url_get(url):
+  curl_handle = pycurl.Curl()
+  curl_handle.setopt(curl_handle.URL, url)
+  curl_handle.setopt(pycurl.WRITEFUNCTION, cback)
+  curl_handle.setopt(pycurl.FOLLOWLOCATION, True)
+
+  try:
+    curl_handle.perform()
+
+  except Exception as exc:
+    logging.warning("Couldn't resolve or connect to %s. %s" % (url, exc))
+
+  curl_handle.close()
+
+active_station_raw = urllib2.urlopen("http://indycast.net/api/stations").read()
+active_station_list = json.loads(active_station_raw)
+tuple_set = [(s['website'], s['callsign']) for s in active_station_list]
+print tuple_set
