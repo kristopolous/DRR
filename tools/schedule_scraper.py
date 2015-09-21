f564ef163c09627097324df6949e670c68f7463b
Chris McKenzie <kristopolous@yahoo.com>
2015-09-21 14:32:48 -0700

    pulling the station list

--- tools/schedule_scraper.py
+++ tools/schedule_scraper.py
@@ -2,4 +2,8 @@
 #
 # Tries to determine the schedule of a station
 #
-# gets the feed from indycast.net/api/stations
+import urllib2
+import json
+
+active_stations_raw = urllib2.urlopen("http://indycast.net/api/stations").read()
+active_station = json.loads(active_stations_raw)
