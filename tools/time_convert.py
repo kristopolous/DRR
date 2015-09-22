1d9adbabf9268384b5e6d701b7a5079dec622dd1
Chris McKenzie <kristopolous@yahoo.com>
2015-09-22 00:36:16 -0700

    compactify the code

--- tools/time_convert.py
+++ tools/time_convert.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python -O
-import sys
 import json
 import lib.ts as TS
 
-all_data = sys.stdin.readlines()[0]
+all_data = raw_input()
 
 res = []
 for data in all_data.split('_'):
   parts = data.strip().split(' ')
-  res.append((TS.to_utc(parts[0], parts[1]),  TS.duration_parse(parts[2]) ))
+  res.append((TS.to_utc(parts[0], parts[1]), TS.duration_parse(parts[2])))
 
 print json.dumps(res)
