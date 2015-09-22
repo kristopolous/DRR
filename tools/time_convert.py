2e773dbd31d22b8a0c81547720fb3309639ffc08
Chris McKenzie <kristopolous@yahoo.com>
2015-09-22 00:06:42 -0700

    returning the time parse as json

--- tools/time_convert.py
+++ tools/time_convert.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python -O
 import sys
+import json
 import lib.ts as TS
 
 all_data = sys.stdin.readlines()[0]
 
+res = []
 for data in all_data.split('_'):
-  parts = data.split(' ')
-  print "%.2f %.2f" % ( TS.to_utc(parts[0], parts[1]),  TS.duration_parse(parts[2]) )
+  parts = data.strip().split(' ')
+  res.append((TS.to_utc(parts[0], parts[1]),  TS.duration_parse(parts[2]) ))
+
+print json.dumps(res)
