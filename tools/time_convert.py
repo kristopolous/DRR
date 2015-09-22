344703f283bf80389be44b8ddc431af5fb8819a1
Chris McKenzie <kristopolous@yahoo.com>
2015-09-21 23:59:40 -0700

    cleanup

--- tools/time_convert.py
+++ tools/time_convert.py
@@ -4,6 +4,7 @@ import lib.misc as misc
 import lib.ts as TS
 
 all_data = sys.stdin.readlines()[0]
+
 for data in all_data.split('_'):
   parts = data.split(' ')
   print "%f %f" % ( TS.to_utc(parts[0], parts[1]),  TS.duration_parse(parts[2]) )
