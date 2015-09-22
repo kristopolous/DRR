9ee7f59fa752bb87d8f84ece58bd4a1a873dfc2e
Chris McKenzie <kristopolous@yahoo.com>
2015-09-22 00:04:01 -0700

    removing a dependency

--- tools/time_convert.py
+++ tools/time_convert.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python -O
 import sys
-import lib.misc as misc
 import lib.ts as TS
 
 all_data = sys.stdin.readlines()[0]
 
 for data in all_data.split('_'):
   parts = data.split(' ')
-  print "%f %f" % ( TS.to_utc(parts[0], parts[1]),  TS.duration_parse(parts[2]) )
+  print "%.2f %.2f" % ( TS.to_utc(parts[0], parts[1]),  TS.duration_parse(parts[2]) )
