75ccdee65a2111819b2b4171f4a87a7cb6a3e768
Chris McKenzie <kristopolous@yahoo.com>
2015-09-23 18:35:18 -0700

    restart through ssh supporting multiple

--- tools/restart_through_ssh.sh
+++ tools/restart_through_ssh.sh
@@ -7,6 +7,9 @@
 #   * Public keys will work
 #   * The server code is in DRR/server
 #   
-station=$1
 
-ssh $station.indycast.net "cd DRR;git pull;./bootstrap.sh;cd server;pkill $station;./indy_server.py -c configs/$station.txt --daemon &" &
+while [ $# -gt 0 ]; do
+  station=$1
+  ssh $station.indycast.net "cd DRR;git pull;./bootstrap.sh;cd server;pkill $station;./indy_server.py -c configs/$station.txt --daemon &" &
+  shift
+done
