56443e52cc224b156e30ec0eea3e11044fae37f2
Chris McKenzie <kristopolous@yahoo.com>
2015-10-22 02:27:21 -0700

    redesign

--- indycast.net/content/index.php
+++ indycast.net/content/index.php
@@ -3,7 +3,7 @@
 <div class='super-main' id="main">
   <h1>A DVR for Radio</h1>
   <span style=display:inline-block>
-  <p>MP3 archives of<br>16 stations</p>
+  <p>MP3 archives<br>16 stations</p>
   <small style=display:block;line-height:1.4em;text-align:center><b>Subscribe to shows<br>Listen to what you missed</b></small>
   </span>
 </div>
