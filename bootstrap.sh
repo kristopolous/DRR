1810108231f99f70857cb957033e4600c75075bf
Chris McKenzie <kristopolous@yahoo.com>
2015-10-27 00:17:12 -0700

    adding a hard upgrade

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,21 @@
 #!/bin/sh
 cd server
 
-sudo apt-get update
+bootstrap() {
+  sudo apt-get update
 
-sudo apt-get -y -f install  \
-    python-pip   python      \
-    python-dev   libxslt1-dev \
-    libxml2-dev  python-pycurl \
-    sqlite3      zlib1g-dev     \
-    build-essential
+  sudo apt-get -y -f install  \
+      python-pip   python      \
+      python-dev   libxslt1-dev \
+      libxml2-dev  python-pycurl \
+      sqlite3      zlib1g-dev     \
+      build-essential
+}
 
+hardupgrade() {
+  rm -r build
+  yes | sudo pip uninstall azure 
+}
+
+# hardupgrade
 pip install --user -r requirements.txt
