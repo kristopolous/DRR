d8f31ce5c7ebc809a832d625792aee6f0a3b46e3
Chris McKenzie <kristopolous@yahoo.com>
2016-02-05 14:56:55 -0800

    putting pids into download logs

--- server/indy_server.py
+++ server/indy_server.py
@@ -98,7 +98,7 @@ def stream_download(callsign, url, my_pid, file_name):
         nl['stream'] = open(file_name, 'w')
 
       except Exception as exc:
-        logging.critical("Unable to open %s. Can't record. Must exit." % file_name)
+        logging.critical("%d: Unable to open %s. Can't record. Must exit." % (nl['pid'], file_name))
         return False
 
     nl['stream'].write(data)
@@ -121,7 +121,7 @@ def stream_download(callsign, url, my_pid, file_name):
 
   except pycurl.error as exc:
     if exc[0] != 23:
-      logging.warning("Couldn't resolve or connect to %s." % url)
+      logging.warning("%d: Couldn't resolve or connect to %s." % (nl['pid'], url))
 
   except:
     pass
