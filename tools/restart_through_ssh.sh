0727c778145fefc8dd4005a322bbdf38558d4e64
Chris McKenzie <kristopolous@yahoo.com>
2016-02-10 03:02:53 -0800

    adding a way to optionally upgrade during the restart

--- tools/restart_through_ssh.sh
+++ tools/restart_through_ssh.sh
@@ -9,10 +9,18 @@
 #   * The server code is in DRR/server
 #   
 
+if [ "$0" == "./upgrade_and_restart_through_ssh.sh" ]; then
+  echo "Doing upgrade"
+  upgrade="rm -f .git/refs/remotes/origin/master.lock;git pull;./bootstrap.sh;"
+else
+  echo "NOT upgrading"
+  upgrade=""
+fi
+
 pid_list=""
 while [ $# -gt 0 ]; do
   station=$1
-  ssh $station.indycast.net "cd DRR;rm -f .git/refs/remotes/origin/master.lock;git pull;./bootstrap.sh;cd server;pkill $station;./indy_server.py -c configs/$station.txt --daemon &" &
+  ssh $station.indycast.net "cd DRR;$upgrade;cd server;pkill $station;./indy_server.py -c configs/$station.txt --daemon &" &
   pid_list=$?" "$pid
   shift
 done
