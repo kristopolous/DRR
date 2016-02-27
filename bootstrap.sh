846d51ab2a6ea99f0033baee16d34d1afba04086
Chris McKenzie <kristopolous@yahoo.com>
2016-02-27 15:46:00 -0800

    updating the bootstrapper for LTR

--- bootstrap.sh
+++ bootstrap.sh
@@ -13,9 +13,17 @@ aptinstall() {
       libxml2-dev  python-pycurl \
       sqlite3      zlib1g-dev     \
       uuid-runtime build-essential \
-      python3-pip  python3        \
+      python3                     \
       libcurl4-gnutls-dev       \
       libgnutls28-dev         
+
+  sudo apt-get -y -f install python3-pip
+
+  # see http://askubuntu.com/questions/412178/how-to-install-pip-for-python-3-in-ubuntu-12-04-lts
+  if [ $? -ne 0 ]; then
+    sudo apt-get -y -f install python3-setuptools
+    sudo easy_install3 pip
+  fi
 }
 
 hardupgrade() {
