72b78e11edf1b8fba153cb0543bba649dffc31e8
Chris McKenzie <kristopolous@yahoo.com>
2016-03-11 15:15:18 -0800

    dump dumping

--- indycast.net/graph.php
+++ indycast.net/graph.php
@@ -1,5 +1,11 @@
 <?php 
 include_once('common.php'); 
 
+$ret = [];
+$qres = $db->query('select * from stats');
+while($row = prune($qres)) {
+  $ret[] = $row;
+}
+echo json_encode($ret, JSON_PRETTY_PRINT);
 ?>
 
