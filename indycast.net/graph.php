885fe245bf2761afeec81816a43acde4045f7206
Chris McKenzie <kristopolous@yahoo.com>
2016-03-11 17:46:09 -0800

    very simple query language

--- indycast.net/graph.php
+++ indycast.net/graph.php
@@ -1,11 +1,29 @@
 <?php 
 include_once('common.php'); 
 
+$vars = sanitize($_GET);
+$where = [];
+if($station = $vars['station']) {
+  $where[] = "callsign = '$station'";
+}
+if($start = $vars['start']) {
+  $where[] = "now > $start";
+}
+if($end = $vars['end']) {
+  $where[] = "now < $end";
+}
+
+$qstr = 'select * from stats';
+if(count($where) > 0) {
+  $qstr .= " where " . implode(' and ', $where);
+}
+
 $ret = [];
-$qres = $db->query('select * from stats');
+$qres = $db->query($qstr);
 while($row = prune($qres)) {
   $ret[] = $row;
 }
+
 echo json_encode($ret, JSON_PRETTY_PRINT);
 ?>
 
