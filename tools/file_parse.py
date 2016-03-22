a0132a5c3ca3313cd42ae25e066c595544493114
Chris McKenzie <kristopolous@yahoo.com>
2016-03-22 15:01:26 -0700

    completing tool to parse and show mp3 info

--- tools/file_parse.py
+++ tools/file_parse.py
@@ -5,3 +5,6 @@ import lib.audio as audio
 for file_name in sys.argv[1:]:
   print("%s:" % file_name)
   print(audio.stream_info(file_name))
+  sig, block = audio.signature(file_name)
+  print({'block_count': len(block), 'format': audio._LASTFORMAT})
+
