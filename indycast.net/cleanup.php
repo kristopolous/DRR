0ef3799e12909f4a3431a42ea4338cd36e5cd9a2
Chris McKenzie <kristopolous@yahoo.com>
2016-04-18 15:15:09 -0700

    more graph cleanup

--- indycast.net/cleanup.php
+++ indycast.net/cleanup.php
@@ -6,24 +6,36 @@ $ret = [];
 $isFirst = true;
 $qres = $db->query($qstr);
 $final = [];
-while($row = prune($qres)) {
 
+while($row = prune($qres)) {
+  $where = [];
+  if(strpos($row['version'], 'v') === 0) {
+    $parts = explode('-', $row['version']);
+    $number = $parts[0];
+    if(count($parts) > 2) {
+      $build = $parts[2];
+    } else {
+      $number = 0;
+    }
+    $row['version'] = trim("$number.$build", 'v');
+    $where[] = "version = '{$row['version']}'";
+  } else {
+    $final[] = $row;
+    continue;
+  }
   foreach(['disk', 'memory', 'latency'] as $key) {
     $row[$key] = round($row[$key], 3);
+    $where[] = "$key = {$row[$key]}";
   }
-  $parts = explode('-', $row['version']);
-  $number = $parts[0];
-  if(count($parts) > 2) {
-    $build = $parts[2];
-  } else {
-    $number = 0;
-  }
-  $row['version'] = trim("$number.$build", 'v');
-  if(strlen($row['uuid'])) {
+  if(strlen($row['uuid']) > 15) {
     $parts = explode('-', $row['uuid']);
     $row['uuid'] = array_pop($parts);
+    $where[] = "uuid = '{$row['uuid']}'";
   }
-  $final[] = array_values($row);
+  $sql = "update stats set " . implode(',', $where) . 
+    " where id = {$row['id']}";
+  $db->query($sql);
+  echo ".";
 }
 echo json_encode($final);
 ?>
