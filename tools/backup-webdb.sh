6fe0e8b9627232b5626f60d7b29dc5abef1696cc
Chris McKenzie <kristopolous@yahoo.com>
2016-04-23 21:01:04 -0700

    fixing a path issue and adding a comment to the web backup script

--- tools/backup-webdb.sh
+++ tools/backup-webdb.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
+# 
+# This is run on indycast.net to backup the main sqlite3 database.
+#
+# You shouldn't ever need it. It's included here because, well,
+# I gotta put it somewhere.
+#
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 base=`dirname $dir`
 fname=`date +"%Y%m%d-%H%M"`.db.lzma
 db=$base/db/main.db
 backup=/tmp/backup.lzma
+
 if [ ! -e $base/misc/backup-secrets.ini ]; then
   echo "Erorr, $base/misc/backup-secrets.ini needs to exist."
   exit -1
@@ -15,7 +22,7 @@ rm -f $backup
 lzma -9 -c $db > $backup
 
 if [ -e $backup ]; then
-  scp -P $port -i $private_key $backup $username@$host:$path/$fname
+  scp -P $port -i $base/$private_key $backup $username@$host:$path/$fname
   rm $backup
 else
   echo "Couldn't make $backup"
