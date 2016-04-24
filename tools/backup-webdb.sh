ed6ae2d2a0e6d7b649b37cd08311d6a027516d4b
Chris McKenzie <kristopolous@yahoo.com>
2016-04-23 17:49:21 -0700

    Working on the web backup

--- tools/backup-webdb.sh
+++ tools/backup-webdb.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 
-fname_base=`date +"%Y%m%d-%H%M"`
-
+fname=`date +"%Y%m%d-%H%M"`.db.lzma
+db=4b/main.db
+backup=/tmp/backup.lzma
 source ../misc/backup-secrets
 
-scp -i $private_key db/main.db $username@$host:$path/$fname
+[ -e $backup ] && rm $backup 
+lzma -c 9 $db $backup
+if [ -e $backup ]; then
+  scp -P $port -i $private_key $backup $username@$host:$path/$fname
+  rm $backup
+else
+  echo "Couldn't make $backup"
+fi
