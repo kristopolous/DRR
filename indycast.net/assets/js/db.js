42b58c945dbcb1ee0a926ffa462bdb8559263627
Chris McKenzie <kristopolous@yahoo.com>
2016-05-05 14:19:58 -0700

    updating deps

--- indycast.net/assets/js/db.js
+++ indycast.net/assets/js/db.js
@@ -430,7 +430,6 @@ var module = module || {},
           filterComp = map(filter, expression());
         }
         //self.fComp = filterComp;
-        //console.log(filterComp);
         
         filterComp_len = filterComp.length;
         set = _filter.call(set, function(row) {
@@ -754,20 +753,51 @@ var module = module || {},
 
         var 
           cList = [], 
+          fnList = [],
           val;
+
         for(var key in arg0) {
-          if(_.isScalar(arg0[key])) {
-            val = arg0[key];
+          val = arg0[key];
+          if(_.isScalar(val)) {
             if(_.isStr(val)) {
               val = '"' + val + '"';
             }
             cList.push("rec['" + key + "']===" + val);
+          } else if(_.isArr(val)) {
+            fnList.push(isin(key, val));
           } else {
             cList.push("equal(rec['" + key + "'],arg0['" + key + "'])");
           }
         };
 
-        return ewrap('rec', 'return ' + cList.join('&&'));
+        if(cList.length) {
+          fnList.push(ewrap('rec,arg0', 'return ' + cList.join('&&')));
+        }
+
+        return function(rec,arg0) {
+          var val, key, fn;
+
+          for(var ix = 0; ix < fnList.length; ix++) {
+            val = fnList[ix];
+
+            if(_.isObj(val)) {
+              key = keys(val)[0];
+              fn = val[key]; 
+
+              if(_.isArr(fn)) {
+                if (indexOf(fn, rec[key]) === -1) {
+                  return false;
+                }
+              } else if(!fn(rec[key])) {
+                return false;
+              }
+            } else if(!val(rec,arg0)) {
+              return false;
+            }
+          }
+
+          return true;
+        }
       }
     }
   }
@@ -1661,4 +1691,4 @@ var module = module || {},
   });
   return DB;
 })();
-DB.version='0.0.2.63-20160423';
+DB.version='0.0.2.73-20160502';
