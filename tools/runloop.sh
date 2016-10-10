b6d6d3af14b3ccefa5e49e180a5a24a69d37a8f2
Chris McKenzie <kristopolous@yahoo.com>
2016-10-10 15:16:01 -0700

    A program to prevent crashes hopefully

---
+++ tools/runloop.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR/../server
+config=configs/$1.txt
+prog=./indy_server.py
+if [ ! -e $config ]; then
+  echo $config not found
+  exit -1
+fi
+
+if [ ! -e $prog ]; then
+  echo $prog not found
+  exit -1
+fi
+
+while [ 0 ]; do
+  cd $DIR/../server
+  $prog -c $config
+  # we need to make sure that there isn't some crazy loop
+  sleep 1
+done
+
