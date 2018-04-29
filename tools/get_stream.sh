bf193b66ccac7e4b1fcb8282c095739f2bd124ea
Chris McKenzie <kristopolous@yahoo.com>
2018-04-28 19:37:18 -0700

    Updating the stream grabber to support the cloud

--- tools/get_stream.sh
+++ tools/get_stream.sh
@@ -2,11 +2,17 @@
 # Gets a remote stream (presuming you have the right credentials)
 # based on a file name
 
-while [ $# -gt 0 ]; do
-  stream=`basename $1`
-  station=$(echo $stream | sed 's/-.*//')
+if [ $# -eq 0 ]; then
+  echo "Usage: $0 station-time.mp3"
+else
+  while [ $# -gt 0 ]; do
+    stream=`basename $1`
+    station=$(echo $stream | sed 's/-.*//')
 
-  [ -e $stream ] || scp $station.indycast.net:radio/$station/streams/$stream .
+    if [ ! -z $stream ]; then 
+      scp $station.indycast.net:radio/$station/streams/$stream . || ./cloud.py -g $stream
+    fi
 
-  shift
-done
+    shift
+  done
+fi
