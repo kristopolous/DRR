6384eb386b67d3e7b75271c00e39457894be4b26
Chris McKenzie <kristopolous@yahoo.com>
2022-05-17 18:45:34 +0000

    trying to fix some service negotiations

--- server/lib/cloud.py
+++ server/lib/cloud.py
@@ -67,7 +67,7 @@ def connect(config=False, service=None):
   if not config: 
     config = misc.config['_private']
 
-  if service == 'sftp' and config.get('sftp') and ((random.random() < 0.20) or not Connection.sftp):
+  if (not service or service == 'sftp') and config.get('sftp') and ((random.random() < 0.20) or not Connection.sftp):
     Connection.counters['sftp'] += 1
     logging.info("<< Reconnecting sftp ({}) >>".format(Connection.counters['sftp']))
 
